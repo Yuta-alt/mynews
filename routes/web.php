014074c8ac7d607bd60b026ec2de66552358bdfc
Yuta-alt <wxrmtr9c@gmail.com>
2020-03-09 10:15:59 +0000

    課題PHP_11

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::get('XXX', 'Admin\AAA@bbb');
 
 // PHP課題09-4
 Route::group(['prefix' => 'admin'], function(){
-  Route::get('admin/profile/create', 'Admin\ProfileController@add'); 
-  Route::get('admin/profile/edit', 'Admin\ProfileController@edit'); 
+  Route::get('profile/create', 'Admin\ProfileController@add'); 
+  Route::get('profile/edit', 'Admin\ProfileController@edit'); 
 });
 
